b57cc39ac7eab0559066c684d32c69d18940b4f6
Meera <26nov93kukku@gmail.com>
2024-08-07 18:47:25 -0400

    Helloworld file created

---
+++ src/main/java/ca/qaguru/basics/HelloWorld.java
@@ -0,0 +1,13 @@
+package ca.qaguru.basics;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+}
+
